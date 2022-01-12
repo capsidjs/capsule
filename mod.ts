35570e30dce1006457f374c062e0c31cb577c6ca
Yoshiya Hinosawa <stibium121@gmail.com>
2022-01-12 17:33:01 +0900

    chore: bump to v0.5.0

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-/*! Capsule v0.4.0 | Copyright 2022 Yoshiya Hinosawa and Capsule contributors | MIT license */
+/*! Capsule v0.5.0 | Copyright 2022 Yoshiya Hinosawa and Capsule contributors | MIT license */
 import { documentReady, logEvent } from "./util.ts";
 
 interface Initializer {
